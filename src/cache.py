44d0fc1dce4cacb575cbd9672476bde16f9a5943
Veb Anand <Veb@Vebs-MacBook-Pro.local>
2017-04-15 22:03:34 -0700

    added cache.py

---
+++ src/cache.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def np_read(path='../data/um/', name='all'):
+    data = np.load(path + name + '.npy')
+    print(name + ' imported:', data.shape, data.dtype)
+    return data
+
+def np_write(path='../data/um/', name='all'):
+    with open(path + name + '.dta') as f:
+        data = [[int(x) for x in line.split()] for line in f.readlines()]
+        data = np.array(data, dtype=np.uint32)
+        np.save(path + 'all', data)
