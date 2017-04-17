43e65bc1cb43a1ad207f7c21dbeaf4be594abefb
lillyluo1995 <lillyluo1995@gmail.com>
2017-04-17 14:10:01 -0700

    Add files via upload

---
+++ avg_avg.py
@@ -0,0 +1,47 @@
+import numpy as np 
+import pandas as pd 
+
+#calculate average user rating across users 
+def avg_user(all, test): 
+	[nrows, ncols] = test.shape
+	pred = np.empty((nrows))
+	group_user = all.groupby('user')[['rating']]
+	avg_user = group_user.aggregate(np.mean)
+
+	for k in range(nrows): 
+		user = test.at[k,'user']
+		pred[k] = np.mean(avg_user.loc[user])
+
+	return(pred)
+
+#calculate average movie rating across movies 
+def avg_movie(all, test):
+	[nrows, ncols] = test.shape
+	pred = np.empty((nrows))
+	group_movie = all.groupby('movie')[['rating']]
+	avg_movie = group_movie.aggregate(np.mean)
+
+	for k in range(nrows): 
+		movie = test.at[k,'movie']
+		pred[k] = np.mean(avg_movie.loc[movie])
+
+	return(pred)
+
+#calculate average of average user rating and movie rating 
+#for each (user, movie) pair 
+def avg_avg(all, test):
+
+	pred_user = avg_user(all, test)
+	pred_movie = avg_movie(all, test)
+	pred_avg = pred_user+pred_movie/2 
+
+	return pred_avg 
+
+#compute predictions 
+all = pd.read_csv('mu/all.dta', delim_whitespace=True, header=None, 
+	names=['user', 'movie', 'time', 'rating'])
+test = pd.read_csv('mu/qual.dta', delim_whitespace=True, header=None, 
+	names=['user', 'movie', 'time'])
+final_pred = avg_avg(all, test)
+
+print(final_pred)
---
+++ linreg.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import pandas as pd 
+
+
+def linreg(all):
+	#linearly regress user and movies on rating, return coefficients 
+	U = all[['user', 'movie']].values
+	R = all[['rating']].values
+	left = np.array(np.linalg.inv(np.dot(np.transpose(U), U)))
+	right = np.array(np.dot(np.transpose(U), R))
+	b = np.array(np.dot(left, right))
+
+	return(b)
+
+all = pd.read_csv('mu/all.dta', delim_whitespace=True, header=None, 
+	names=['user', 'movie', 'time', 'rating'])
+test = pd.read_csv('mu/qual.dta', delim_whitespace=True, header=None, 
+	names=['user', 'movie', 'time'])
+
+coef = linreg(all)
+test_U = test[['user', 'movie']].values
+pred = np.transpose(np.dot(test_U, coef))[0]
+
+print(pred)
